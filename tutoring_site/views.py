134b7ec693d3098b78548342aaefeada4c2c2424
Advay Thalanchery <advay.thalanchery@gmail.com>
2026-04-03 15:06:14 -0400

    adding google form links

--- tutoring_site/views.py
+++ tutoring_site/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'google_form_url': 'https://forms.gle/REPLACE_WITH_YOUR_FORM_ID',
-        'google_embed_url': 'https://docs.google.com/forms/d/e/REPLACE_WITH_YOUR_FORM_ID/viewform?embedded=true',
+        'google_form_url': 'https://docs.google.com/forms/d/e/1FAIpQLSe4TfMRrkg1tpd_hZDVpVYVD29-SisH-7n_jM2KlTD3PDUK2g/viewform?usp=header',
+        'google_embed_url': 'https://docs.google.com/forms/d/e/1FAIpQLSe4TfMRrkg1tpd_hZDVpVYVD29-SisH-7n_jM2KlTD3PDUK2gREPLACE_WITH_YOUR_FORM_ID/viewform?embedded=true',
     }
     return render(request, 'tutoring_site/home.html', context)
